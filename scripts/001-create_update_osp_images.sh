5a050754d653026a62a5076753ada44b9ff5f31c
Federico Iezzi <fiezzi@redhat.com>
2019-08-16 16:27:11 +0000

    Undercloud config + CTRL instackenv + basic script for image creation and instackenv import

---
+++ scripts/001-create_update_osp_images.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "#### Removing old image directory content"
+rm -rf ~/images
+
+echo "#### Updating local overcloud image packages to the latest version"
+sudo rpm -q rhosp-director-images >/dev/null 2>&1 || sudo yum install -y rhosp-director-images
+sudo rpm -q rhosp-director-images-ipa >/dev/null 2>&1 || sudo yum install -y rhosp-director-images-ipa
+sudo yum update -y rhosp-director-images rhosp-director-images-ipa
+
+echo "#### Extract new overcloud images"
+mkdir ~/images
+cd ~/images
+for i in /usr/share/rhosp-director-images/overcloud-full-latest-13.0.tar /usr/share/rhosp-director-images/ironic-python-agent-latest-13.0.tar; do tar -xvf $i; done
+
+echo "#### Uploading updated overcloud image to the Undercloud's Glance Registry"
+source ~/stackrc
+openstack overcloud image upload --image-path ~/images/ --update-existing
+
+exit 0
---
+++ scripts/002-import_instackenv_nodes.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Change directory to where this script is located
+# Given the above assumption, all path are local ones
+cd $(dirname $(readlink -f $0))
+
+source ~/stackrc
+openstack overcloud node import ../undercloud/instackenv.json
+openstack baremetal node list|awk '/power on/ {print $2}'|xargs -n1 openstack baremetal node power off
+openstack overcloud node introspect --all-manageable --provide
+openstack overcloud node configure $(openstack baremetal node list -c UUID -f value)
+
+exit 0
