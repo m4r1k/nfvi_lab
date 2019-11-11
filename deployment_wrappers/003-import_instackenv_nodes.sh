f35f121c7b42dbcd981b0dfb39b794936736561b
Federico Iezzi <fiezzi@redhat.com>
2019-11-11 14:46:26 +0100

    Ensure to execute node power down only if any are up

--- deployment_wrappers/003-import_instackenv_nodes.sh
+++ deployment_wrappers/003-import_instackenv_nodes.sh
@@ -6,7 +6,7 @@ cd $(dirname $(readlink -f $0))
 
 source ~/stackrc
 openstack overcloud node import ../undercloud/instackenv.json
-openstack baremetal node list --provision-state manageable|awk '/power on/ {print $2}'|xargs -n1 openstack baremetal node power off
+openstack baremetal node list --provision-state manageable|awk '/power on/ {print $2}'|xargs -n 1 -P 4 -r openstack baremetal node power off
 openstack overcloud node introspect --all-manageable --provide
 openstack overcloud node configure $(openstack baremetal node list -c UUID -f value)
 
