5ca18e40eba4b6baedf5303550cd4f1a18efe3ea
Federico Iezzi <fiezzi@redhat.com>
2020-02-25 18:01:36 +0100

    [oc][Ceph] many improvments
     - Enable Beast (through workaround)
     - Allow Public URL in RadosGW
     - Enable Cinder Backup dashboard in Horizon
     - Support all the base OpenStack roles

---
+++ overcloud/extraconfig/post-config/scripts/beastrgw.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+grep -q "rgw frontends = civetweb" /etc/ceph/ceph.conf
+if [[ "$?" == "0" ]]; then
+	sed -e "s/rgw frontends = civetweb/rgw frontends = beast/g" -i /etc/ceph/ceph.conf
+	docker ps --filter name=rgw --quiet | xargs -n1 -r docker restart
+fi
+
+exit 0
