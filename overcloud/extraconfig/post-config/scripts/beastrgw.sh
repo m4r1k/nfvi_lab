3152f8fca21abbb64f57bba2e1c0d3061992c774
Federico Iezzi <fiezzi@redhat.com>
2020-02-25 18:43:18 +0100

    [oc] set static admin password

--- overcloud/extraconfig/post-config/scripts/beastrgw.sh
+++ overcloud/extraconfig/post-config/scripts/beastrgw.sh
@@ -2,7 +2,7 @@
 
 grep -q "rgw frontends = civetweb" /etc/ceph/ceph.conf
 if [[ "$?" == "0" ]]; then
-	sed -e "s/rgw frontends = civetweb/rgw frontends = beast/g" -i /etc/ceph/ceph.conf
+	sed -e "s/rgw frontends = civetweb port/rgw frontends = beast endpoint/g" -i /etc/ceph/ceph.conf
 	docker ps --filter name=rgw --quiet | xargs -n1 -r docker restart
 fi
 
