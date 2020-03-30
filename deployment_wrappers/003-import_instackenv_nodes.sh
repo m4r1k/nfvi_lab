5db841394e6441219044831ea999b3541456b5a2
Federico Iezzi <fiezzi@redhat.com>
2020-03-30 15:57:03 +0000

    [osp16] adaptations for wrapper 003

--- deployment_wrappers/003-import_instackenv_nodes.sh
+++ deployment_wrappers/003-import_instackenv_nodes.sh
@@ -8,6 +8,6 @@ source ~/stackrc
 openstack overcloud node import ../undercloud/instackenv.json
 openstack baremetal node list --provision-state manageable|awk '/power on/ {print $2}'|xargs -n 1 -P 4 -r openstack baremetal node power off
 openstack overcloud node introspect --all-manageable --provide
-openstack baremetal node list -c UUID -f value|xargs -n 1 -P 1 -r openstack overcloud node configure
+openstack baremetal node list -c UUID -f value|xargs -n 10 -P 1 -r openstack overcloud node configure
 
 exit 0
