f3e4ef1f7aefe82869252f0ae19de043b5df45dd
Federico Iezzi <fiezzi@redhat.com>
2020-04-02 07:59:07 +0000

    Revert "[validation][osp16] new Train flavor property"
    This reverts commit 279eca908d1882ab9a2176f812d3dbeddb05a4e8.

--- validation/basic_ovs-dpdk/create_gre.sh
+++ validation/basic_ovs-dpdk/create_gre.sh
@@ -31,8 +31,8 @@ openstack flavor show nfv >/dev/null 2>&1 || openstack flavor create \
 	--disk 10 \
 	--vcpus 7 \
 	--public \
-	--property resources:PCPU=7 \
-	--property trait:HW_CPU_HYPERTHREADING=required \
+	--property hw:cpu_policy=dedicated \
+	--property hw:cpu_thread_policy=isolate \
 	--property hw:mem_page_size=large \
 	--property hw:numa_mempolicy=strict \
 	--property hw:emulator_threads_policy=share \
