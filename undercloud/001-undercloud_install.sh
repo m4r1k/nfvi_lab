2c88afd19bd3b5a9ff99caf9d22da5caa2083096
Federico Iezzi <fiezzi@redhat.com>
2020-10-01 14:25:25 +0000

    [osp16][uc] new repos for 16.1 and minor enhancements

--- undercloud/001-undercloud_install.sh
+++ undercloud/001-undercloud_install.sh
@@ -29,23 +29,27 @@ sudo subscription-manager clean >/dev/null 2>&1
 
 sudo subscription-manager register --username=${_USERNAME} --password=${_PASSWORD} || exit 1
 sudo subscription-manager attach --pool=${_POOL} || exit 1
-sudo subscription-manager release --set=8.1
+sudo subscription-manager release --set=8.2
 sudo subscription-manager repos \
 --disable "*" \
---enable rhel-8-for-x86_64-baseos-rpms \
---enable rhel-8-for-x86_64-appstream-rpms \
---enable rhel-8-for-x86_64-highavailability-rpms \
+--enable rhel-8-for-x86_64-baseos-eus-rpms \
+--enable rhel-8-for-x86_64-appstream-eus-rpms \
+--enable rhel-8-for-x86_64-highavailability-eus-rpms \
 --enable rhel-8-for-x86_64-nfv-rpms \
 --enable rhel-8-for-x86_64-rt-rpms \
---enable rhel-8-for-x86_64-supplementary-rpms \
---enable ansible-2.8-for-rhel-8-x86_64-rpms \
+--enable rhel-8-for-x86_64-supplementary-eus-rpms \
+--enable ansible-2.9-for-rhel-8-x86_64-rpms \
 --enable advanced-virt-for-rhel-8-x86_64-rpms \
 --enable satellite-tools-6.5-for-rhel-8-x86_64-rpms \
---enable openstack-16-for-rhel-8-x86_64-rpms \
+--enable openstack-16.1-for-rhel-8-x86_64-rpms \
 --enable fast-datapath-for-rhel-8-x86_64-rpms \
 --enable rhceph-4-tools-for-rhel-8-x86_64-rpms || exit 1
 
 sudo dnf makecache
+sudo dnf module disable -y container-tools:rhel8
+sudo dnf module enable -y container-tools:2.0
+sudo dnf module disable -y virt:rhel
+sudo dnf module enable -y virt:8.2
 sudo dnf upgrade -y
 sudo dnf install -y vim git bash-completion
 sudo dnf install -y python3-tripleoclient ceph-ansible
@@ -53,10 +57,20 @@ sudo dnf install -y python3-tripleoclient ceph-ansible
 sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
 sudo dnf config-manager --set-disabled epel
 sudo dnf --enablerepo=epel install screen -y
+cat > ~/.screenrc << EOF
+hardstatus alwayslastline
+hardstatus string '%{= kG}[ %{G}%H %{g} ][%= %{= kw}%?%-Lw%?%{r} (%{W}%n*%f%t%?(%u)%?%{r})%{w}%?%+Lw%?%?%= %{g}][%{B} %d/%m %{W}%c %{g}]'
+termcapinfo xterm* ti@:te@
+defscrollback 90000
+EOF
 
 # https://github.com/amix/vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_basic_vimrc.sh
+cd ~/.vim_runtime
+git pull --rebase
+python3 update_plugins.py
+cd
 
 openstack complete > ~/openstack
 sudo mv ~/openstack /etc/bash_completion.d/openstack
